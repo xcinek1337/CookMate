afa8bc7840a8cc1e3f49a516f2f6e3fcd3786405
Marcin <marcinchodkowski004@wp.pl>
2024-08-10 21:32:47 +0200

    meals by region view

---
+++ src/app/regional/[region]/page.tsx
@@ -0,0 +1,34 @@
+import Flag from 'react-world-flags';
+import Link from 'next/link';
+import { regionsArray } from '@/const';
+import MealList from '@/components/MealView/MealList';
+
+export default async function MealsPage({ params }: { params: { region: string } }) {
+	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${params.region}`);
+	const { meals } = await response.json();
+
+	return (
+		<section>
+			<h3 className='my-4 text-center font-semibold text-gray-700 px-4'>Browse by regional cuisine</h3>
+			<div className='flex items-center just gap-7 px-4 bg-gray-600 rounded-md overflow-x-scroll max-w-7xl mx-auto'>
+				{regionsArray.map((region) => {
+					return (
+						<Link
+							className={`py-1.5 px-2 border-2 ${
+								params.region === region.strArea ? 'border-2 border-yellow-300' : 'border-transparent'
+							}  hover:border-yellow-300 transition`}
+							key={region.code}
+							href={`/regional/${region.strArea}`}
+						>
+							<div className='w-10 mx-auto py-1'>
+								<Flag code={region.code} />
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+
+			<MealList meals={meals} />
+		</section>
+	);
+}
