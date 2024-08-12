e1722a94d565af005c1d593c92b45091318e0fcb
Marcin <marcinchodkowski004@wp.pl>
2024-08-12 15:00:47 +0200

    add to favaoruite server function

--- src/actions/addToFav.ts
+++ src/actions/addToFav.ts
@@ -5,11 +5,31 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async function addToFav(mealInfo: MealProps) {
-	await prisma.favourites.create({
-        data:{
-            meal:mealInfo,
-            // id_user:
-        }
-    });
+export default async function addToFav(mealInfo: MealProps, userId: any) {
+	const existingMeal = await prisma.favourites.findFirst({
+		where: {
+			meal: {
+				path: ['idMeal'],
+				equals: mealInfo.idMeal,
+			},
+			id_user: userId,
+		},
+	});
+
+	if (existingMeal) {
+		await prisma.favourites.delete({
+			where: {
+				id: existingMeal.id,
+			},
+		});
+		return 'deleted from favourites';
+	} else {
+		await prisma.favourites.create({
+			data: {
+				meal: mealInfo,
+				id_user: userId,
+			},
+		});
+		return 'added to favourites';
+	}
 }
